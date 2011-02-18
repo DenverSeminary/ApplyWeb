05191e308b7a08bf3f5634b7c29b801f08e46336
Seth Washeck <swasheck@gmail.com>
2011-02-18 15:02:30 -0700

    Cleaned up some stuff. Still using httplib2 for Tycoon interactions

--- connection.py
+++ connection.py
@@ -3,35 +3,24 @@
 	SFTP Interface
 
 '''
-import paramiko, time, dbclient, config
+import paramiko, time, config, dbi
 from datetime import datetime
 
 class SFTPConnection():
 	
 	def get_file_list(self):
+		db = dbi.dbi()
+		db.conn()
 		transport = paramiko.Transport( (config.get_config('sftp','SERVER'), int(config.get_config('sftp','PORT'))) )
 		transport.connect(username = config.get_config('sftp','USERNAME'), password = config.get_config('sftp','PASSWORD'))
 		sftp = paramiko.SFTPClient.from_transport(transport)
 		files = []
-		attrs = sftp.listdir_attr(config.get_config('sftp','PATH'))
-		db = dbclient.Client()
-		for attr in attrs:
-			if db.get('files.kch',attr.filename) is None:
-				# if the filename is not in the database, we assume that the file has not been processed. 
+		attrs = sftp.listdir_attr(config.get_config('sftp','PATH'))		
+		for attr in attrs:								
+			if db.get(attr.filename) is None:
 				sftp.get(config.get_config('sftp','PATH') + attr.filename, attr.filename + ".csv")
-				# then we get the file
-				db.set('files.kch', attr.filename, str(int(time.mktime(datetime.timetuple(datetime.now())))))
-				# then we save the filename in the database so that we don't pull it again.
-				files.append(attr.filename + '.csv')
-				# finally, we put it in the list 
-			'''
-			if db.get('files.kch', attr.filename) == None:
-				fil = {}
-				fil['filename'] = attr.filename
-				fil['filedate'] = attr.st_mtime
-				files.append(fil)			
-				sftp.get(PATH + attr.filename, attr.filename + ".csv")
-			'''
+				db.set(attr.filename, str(int(time.mktime(datetime.timetuple(datetime.now())))))
+				files.append(attr.filename + ".csv")
 		sftp.close()
-		transport.close()		
-		return files
+		transport.close()				
+		return files	
